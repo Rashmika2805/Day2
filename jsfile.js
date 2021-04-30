d1c010f7ff527383996728ab5c682ce378f8aa23
Rashmika2805 <67779587+Rashmika2805@users.noreply.github.com>
2021-04-30 14:35:52 +0530

    Update jsfile.js

--- jsfile.js
+++ jsfile.js
@@ -1,3 +1,12 @@
 function display(val){
     document.getElementById("dis").value+=val;
 }
+function calculate(){
+    var a=document.getElementById("dis").value;
+    var b=eval(a);
+    if(b==undefined){
+        b=null;
+    }
+    document.getElementById("dis").value=b;
+}
+
